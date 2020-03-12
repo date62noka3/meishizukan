aedf1c1ebee95440ccd1f3b1413a8a0bf26305cb
Argate Logas <outsameunit@gmail.com>
2020-03-12 11:21:42 +0900

    レイアウト実装

--- app/src/main/java/com/example/meishizukan/activity/PhotosViewActivity.kt
+++ app/src/main/java/com/example/meishizukan/activity/PhotosViewActivity.kt
@@ -1,13 +1,58 @@
 package com.example.meishizukan.activity
 
+import android.database.sqlite.SQLiteDatabase
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.meishizukan.R
+import com.example.meishizukan.util.DbHelper
+import com.google.android.gms.ads.AdListener
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.MobileAds
+import kotlinx.android.synthetic.main.activity_photos_view.*
 
 class PhotosViewActivity : AppCompatActivity() {
+
+    private var personId = 0
+
+    private val dbHelper = DbHelper(this)
+    private lateinit var readableDB: SQLiteDatabase
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_photos_view)
+
+        readableDB = dbHelper.readableDatabase
+
+        //AdMob初期化
+        MobileAds.initialize(this) {}
+        val adRequest = AdRequest.Builder()
+            .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+            .build()
+        adView.loadAd(adRequest)
+        adView.adListener = object: AdListener() {
+            override fun onAdLoaded() {}
+            override fun onAdFailedToLoad(errorCode : Int) {}
+            override fun onAdOpened() {}
+            override fun onAdClicked() {}
+            override fun onAdLeftApplication() {}
+            override fun onAdClosed() {}
+        }
+
+        backButton.setOnClickListener{
+            super.onBackPressed()
+        }
+
+        personId = intent.getIntExtra("PERSON_ID",0)
+    }
+
+    override fun onResume(){
+        super.onResume()
+        adView.resume()
+    }
+
+    override fun onDestroy(){
+        adView.destroy()
+        readableDB.close()
+        super.onDestroy()
     }
 }
