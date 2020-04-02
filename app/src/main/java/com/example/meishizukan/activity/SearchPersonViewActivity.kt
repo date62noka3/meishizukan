8d6db87a0fefa57c1284f8699162bf13244eb798
Argate Logas <outsameunit@gmail.com>
2020-04-02 18:05:06 +0900

    junitを追加

--- app/src/main/java/com/example/meishizukan/activity/SearchPersonViewActivity.kt
+++ app/src/main/java/com/example/meishizukan/activity/SearchPersonViewActivity.kt
@@ -10,7 +10,6 @@ import android.os.Handler
 import android.provider.BaseColumns
 import android.text.Editable
 import android.text.TextWatcher
-import android.util.AttributeSet
 import android.util.Log
 import android.view.KeyEvent
 import android.view.View
@@ -493,7 +492,7 @@ class SearchPersonViewActivity : AppCompatActivity() {
         scrollToTop(false)
 
         var keyword = searchEditText.text.toString()
-        keyword = PhoneticName.hiraganaToKatakana(keyword)
+        keyword = hiraganaToKatakana(keyword)
 
         val sql = createSearchSQL(keyword)
 
