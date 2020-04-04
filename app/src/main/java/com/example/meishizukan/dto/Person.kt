814ffd2c2583c55ff528abeb6cd015ded7fdfd11
Argate Logas <outsameunit@gmail.com>
2020-04-04 12:44:44 +0900

    dtoを修正 classをdata classに変更

--- app/src/main/java/com/example/meishizukan/dto/Person.kt
+++ app/src/main/java/com/example/meishizukan/dto/Person.kt
@@ -1,24 +1,10 @@
 package com.example.meishizukan.dto
 
-class Person(
-    id:Int,
-    name:String,
-    phoneticName:String,
-    sex:Int,
-    organizationName:String,
-    note:String
-) {
-    private val id = id
-    private val name = name
-    private val phoneticName = phoneticName
-    private val sex = sex
-    private val organizationName = organizationName
-    private val note = note
-
-    fun getId():Int{ return id }
-    fun getName():String{ return name }
-    fun getPhoneticName():String{ return phoneticName }
-    fun getSex():Int{ return sex }
-    fun getOrganizationName():String{ return organizationName }
-    fun getNote():String{ return note }
-}
+data class Person(
+    val id:Int,
+    val name:String,
+    val phoneticName:String,
+    val sex:Int,
+    val organizationName:String,
+    val note:String
+)
--- app/src/main/java/com/example/meishizukan/dto/Photo.kt
+++ app/src/main/java/com/example/meishizukan/dto/Photo.kt
@@ -1,18 +1,30 @@
 package com.example.meishizukan.dto
 
-class Photo(
-    id:Int,
-    hashedBinary:ByteArray,
-    binary:ByteArray,
-    createdOn:String
+data class Photo(
+    val id:Int,
+    val hashedBinary:ByteArray,
+    val binary:ByteArray,
+    val createdOn:String
 ) {
-    private val id = id
-    private val hashedBinary = hashedBinary
-    private val binary = binary
-    private val createdOn = createdOn
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
 
-    fun getId():Int{ return id }
-    fun getHashedBinary():ByteArray{ return hashedBinary }
-    fun getBinary():ByteArray{ return binary }
-    fun getCreatedOn():String{ return createdOn }
+        other as Photo
+
+        if (id != other.id) return false
+        if (!hashedBinary.contentEquals(other.hashedBinary)) return false
+        if (!binary.contentEquals(other.binary)) return false
+        if (createdOn != other.createdOn) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = id
+        result = 31 * result + hashedBinary.contentHashCode()
+        result = 31 * result + binary.contentHashCode()
+        result = 31 * result + createdOn.hashCode()
+        return result
+    }
 }
